e56d436da40d903b82090276a3ffd494760b53d3
kauaumonougueira <kaua_kna@hotmail.com>
2022-05-20 12:27:18 -0300

    entities client finalizada

---
+++ src/entities/Client.java
@@ -0,0 +1,13 @@
+package entities;
+
+public class Client{
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
